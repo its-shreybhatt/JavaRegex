239c83a701ded3409a320db99d94351710f946e0
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-15 15:47:43 +0530

    Added : Code to check for Last Name using Regex

--- src/Regex.java
+++ src/Regex.java
@@ -6,11 +6,20 @@ public class Regex {
         System.out.println("Welcome to Regex Coding");
         Regex regex = new Regex();
         regex.firstName();
+        regex.lastName();
     }
     public void firstName(){
-        Pattern pattern = Pattern.compile("[A-Z][a-z]{2,}");
-        Matcher matcher = pattern.matcher("Shrey");
-        boolean value = matcher.matches();
-        System.out.println(value);
+        Pattern patternOne = Pattern.compile("[A-Z][a-z]{2,}");
+        Matcher matcherOne = patternOne.matcher("Shrey");
+        boolean valueOne = matcherOne.matches();
+        System.out.println("Result for First Name = " +valueOne);
     }
+    public void lastName(){
+        Pattern patternTwo = Pattern.compile("[A-z][A-za-z]{2,}");
+        Matcher matcherTwo = patternTwo.matcher("BHATT");
+        boolean valueTwo = matcherTwo.matches();
+        System.out.println("Result for Last Name = " +valueTwo);
+    }
+
+
 }
