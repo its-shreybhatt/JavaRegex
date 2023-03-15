6ed2fd853c796570c302fe597a97ed50a10a85bb
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-15 15:55:42 +0530

    Added : Code to check for email-Id using Regex

--- src/Regex.java
+++ src/Regex.java
@@ -7,19 +7,25 @@ public class Regex {
         Regex regex = new Regex();
         regex.firstName();
         regex.lastName();
+        regex.emailId();
     }
-    public void firstName(){
+
+    public void firstName() {
         Pattern patternOne = Pattern.compile("[A-Z][a-z]{2,}");
         Matcher matcherOne = patternOne.matcher("Shrey");
         boolean valueOne = matcherOne.matches();
-        System.out.println("Result for First Name = " +valueOne);
+        System.out.println("Result for First Name = " + valueOne);
     }
-    public void lastName(){
+
+    public void lastName() {
         Pattern patternTwo = Pattern.compile("[A-z][A-za-z]{2,}");
         Matcher matcherTwo = patternTwo.matcher("BHATT");
         boolean valueTwo = matcherTwo.matches();
-        System.out.println("Result for Last Name = " +valueTwo);
+        System.out.println("Result for Last Name = " + valueTwo);
     }
 
-
+    public void emailId() {
+        System.out.println("Result for emailId = " +Pattern.compile("[a-z]+[.]?[a-z]{0,}[@][a-z]+[.][a-z]{2,4}[.]?[a-z]{0,2}")
+                .matcher("abc@bridgelabz.com").matches());
+    }
 }
