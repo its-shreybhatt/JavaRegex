84bf08b922b1c090aa1e37f2f272b8ade681d247
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-15 15:42:17 +0530

    Added : Code to check for First Name using Regex

--- src/Regex.java
+++ src/Regex.java
@@ -1,5 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Coding");
+        Regex regex = new Regex();
+        regex.firstName();
+    }
+    public void firstName(){
+        Pattern pattern = Pattern.compile("[A-Z][a-z]{2,}");
+        Matcher matcher = pattern.matcher("Shrey");
+        boolean value = matcher.matches();
+        System.out.println(value);
     }
 }
